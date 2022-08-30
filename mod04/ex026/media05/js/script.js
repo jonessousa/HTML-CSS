3e5104021e6dfa508ebc36f3119a44b66c13d060
Jones Sousa <106538892+jonessousa@users.noreply.github.com>
2022-08-30 09:03:23 -0300

    media query

--- mod04/ex026/media05/js/script.js
+++ mod04/ex026/media05/js/script.js
@@ -1,3 +1,11 @@
+function tamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
 function clickMenu(){
     if (menu.style.display == 'block') {
         menu.style.display = 'none'
